7548987461f11333b3b5fc190bbaa918191ed12f
SumiChalil <sumisha.chalil@gmail.com>
2024-08-25 15:54:36 +0530

    adding new models to model

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print("Add all ML related code here")
+
+def model1():
+    print("Adding code for ML1")
+
+def model2():
+    print("Adding code for ML2")
+
+
+def model3():
+    print("Adding code for ML3")
